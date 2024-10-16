ad9038bd1f5a2d7ae89331ee33f51d17bbf0d0e7
Enrique-ZA <60000947+Enrique-ZA@users.noreply.github.com>
2024-10-16 11:56:30 +0200

    Update uyv.js | 16-10-24

--- uyv.js
+++ uyv.js
@@ -1,4 +1,6 @@
 const uyvType = {};
+uyvType.Image = HTMLImageElement;
+uyvType.ImageData = ImageData;
 function uyvIsNumber(value) {
   return typeof value === "number" && isFinite(value);
 }
@@ -40,10 +42,24 @@ let uyvHeight;
 let uyvCurrentFillColor = null;
 let uyvCurrentStrokeColor = null;
 let uyvCurrentStrokeWeight = 1;
+let uyvDeltaTime = 0;
+let uyvPrevTimestamp = 0;
+let uyvKey = "";
+// Main Functions Start --------------------------------------------------------
+/**
+ * Creates the main drawing canvas. It should only be
+ * called once at the beginning of uyvStart()
+ * @returns {{canvas: CanvasRenderingContext2D, screen: HTMLCanvasElement}}
+ *
+ * @param {uyvType.uint16} w
+ * @param {uyvType.uint16} h
+ */
 function uyvCreateScreen(w, h) {
-  let canvasID = document.getElementById("game");
-  if (canvasID === null) {
-    throw new Error("No canvas with id game is found");
+  /** @type {HTMLCanvasElement | null} */
+  const canvas = document.createElement("canvas");
+  canvas.id = "uyvCanvas";
+  if (canvas === null) {
+    throw new Error("No canvas with id uyvCanvas is found");
   }
   if (w < 0 || w > 7680) {
     throw new Error("Screen width needs to be between 0 and 7680 (inclusive)");
@@ -51,25 +67,100 @@ function uyvCreateScreen(w, h) {
   if (h < 0 || h > 4320) {
     throw new Error("Screen height needs to be between 0 and 4320 (inclusive)");
   }
-  canvasID.width = w;
-  canvasID.height = h;
-  const uyvCanvasCtx = canvasID.getContext("2d");
+  canvas.width = w;
+  canvas.height = h;
+  document.body.appendChild(canvas);
+  const uyvCanvasCtx = canvas.getContext("2d");
   if (uyvCanvasCtx === null) {
     throw new Error("2D context error.");
   }
-  const uyvScreen = canvasID;
+  const uyvScreen = canvas;
   uyvWidth = uyvCanvasCtx.canvas.width;
   uyvHeight = uyvCanvasCtx.canvas.height;
   return { canvas: uyvCanvasCtx, screen: uyvScreen };
 }
 function uyvInitializeEngine() {
-  if (typeof window.uyvStart === "function") {
-    window.uyvStart();
+  if (typeof window.uyvPreLoad === "function") {
+    Promise.resolve(window.uyvPreLoad())
+      .then(() => {
+        uyvStartEngine();
+      })
+      .catch((error) => {
+        console.error("Error in uyvPreLoad:", error);
+      });
   } else {
+    uyvStartEngine();
+  }
+}
+function uyvHandleKeyDown(e) {
+  uyvKey = e.key;
+  if (typeof window.uyvKeyDown === "function") {
+    window.uyvKeyDown();
+  }
+}
+function uyvHandleKeyUp(e) {
+  const releasedKey = e.key;
+  if (typeof window.uyvKeyUp === "function") {
+    uyvKey = releasedKey;
+    window.uyvKeyUp();
+  }
+  if (uyvKey === releasedKey) {
+    uyvKey = "";
+  }
+}
+/**
+ * Starts the engine by calling uyvStart and initiating the main loop.
+ * @ignore
+ */
+function uyvStartEngine() {
+  if (typeof window.uyvStart !== "function") {
     console.error(
       "No uyvStart function defined. Define function uyvStart() in your script.",
     );
+    return;
   }
+  if (typeof window.uyvUpdate !== "function") {
+    console.error(
+      "No uyvUpdate function defined. Define function uyvUpdate() in your script.",
+    );
+    return;
+  }
+  if (typeof window.uyvDraw !== "function") {
+    console.error(
+      "No uyvDraw function defined. Define function uyvDraw() in your script.",
+    );
+    return;
+  }
+  window.uyvStart();
+  window.requestAnimationFrame(uyvMainLoop);
+  window.addEventListener("keydown", uyvHandleKeyDown);
+  window.addEventListener("keyup", uyvHandleKeyUp);
+}
+function uyvMainLoop(timestamp) {
+  uyvDeltaTime = (timestamp - uyvPrevTimestamp) / 1000;
+  uyvPrevTimestamp = timestamp;
+  window.uyvUpdate();
+  window.uyvDraw();
+  window.requestAnimationFrame(uyvMainLoop);
+}
+function uyvLoadImageAndData(url) {
+  const image = new Image();
+  image.src = url;
+  return new Promise((resolve, reject) => {
+    image.onload = () => {
+      const canvas = document.createElement("canvas");
+      canvas.width = image.width;
+      canvas.height = image.height;
+      const ctx = canvas.getContext("2d");
+      if (ctx === null) {
+        throw new Error("The browser does not support this game engine.");
+      }
+      ctx.drawImage(image, 0, 0);
+      const imgData = ctx.getImageData(0, 0, image.width, image.height);
+      resolve({ image: image, data: imgData });
+    };
+    image.onerror = reject;
+  });
 }
 function uyvGetWidth(uyvCanvasCtx) {
   return uyvCanvasCtx.canvas.width;
@@ -109,11 +200,17 @@ class uyvVector2D {
     add( other) {
     return new uyvVector2D(this.x + other.x, this.y + other.y);
   }
+    lengthSqrt() {
+    return this.x * this.x + this.y * this.y;
+  }
     length() {
     return Math.sqrt(this.x * this.x + this.y * this.y);
   }
     rotate90() {
     return new uyvVector2D(-this.y, this.x);
+  }
+    distanceToSqrt( other) {
+    return other.sub(this).lengthSqrt();
   }
     distanceTo( other) {
     return other.sub(this).length();
@@ -134,6 +231,34 @@ class uyvVector2D {
     dot(other) {
     return this.x * other.x + this.y * other.y;
   }
+}
+function xoshiro128ss(a, b, c, d) {
+  return function() {
+    let t = b << 9,
+      r = b * 5;
+    r = ((r << 7) | (r >>> 25)) * 9;
+    c ^= a;
+    d ^= b;
+    b ^= c;
+    a ^= d;
+    c ^= t;
+    d = (d << 11) | (d >>> 21);
+    return (r >>> 0) / 4294967296;
+  };
+}
+function uyvRandom(min, max) {
+  const seedgen = () => (Math.random() * 2 ** 32) >>> 0;
+  const rand = xoshiro128ss(seedgen(), seedgen(), seedgen(), seedgen());
+  if (!uyvIsNumber(min) || !uyvIsNumber(max)) {
+    console.warn("uyvRandom: params have to be numbers.");
+    return rand();
+  }
+  if (min > max) {
+    const tmp = min;
+    min = max;
+    max = tmp;
+  }
+  return rand() * (max - min) + min;
 }
 function uyvPush(uyvCanvasCtx) {
   uyvCanvasCtx.save();
